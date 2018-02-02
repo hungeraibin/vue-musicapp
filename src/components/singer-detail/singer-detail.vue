de79a74b58a3d1a7d5b1002650f11a6c7a0e32e1
王爱彬 <hungeraibin@126.com>
2018-02-02 17:48:26 +0800

    歌手详情数据处理

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,33 +6,70 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { getSingerDetail } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import { createSong } from 'common/js/song'
 
 export default {
+  data() {
+    return {
+      songs: []
+    }
+  },
   computed: {
     ...mapGetters([
       'singer'
     ])
   },
   created() {
-    console.log(this.singer)
+    this._getDetail()
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
+        }
+      })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach((item) => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
   }
+
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import "~common/stylus/variable"
+@import '~common/stylus/variable';
 
-  .singer-detail 
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
+.singer-detail {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: $color-background;
+}
 
-  .slider-enter-active, .slider-leave-active 
-    transition: all 0.3s
-  .slider-enter, .slider-leave
-    transform: translate3d(100%, 0, 0)
+.slider-enter-active, .slider-leave-active {
+  transition: all 0.3s;
+}
+
+.slider-enter, .slider-leave {
+  transform: translate3d(100%, 0, 0);
+}
 </style>
