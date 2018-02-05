9a743728730f1a085fd95d94e90cf1399f29e907
王爱彬 <hungeraibin@126.com>
2018-02-05 13:55:47 +0800

    播放器数据Vuex

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -32,6 +32,7 @@ export default {
       }
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
+          console.log(res.data.list)
           this.songs = this._normalizeSongs(res.data.list)
           console.log(this.songs)
         }
