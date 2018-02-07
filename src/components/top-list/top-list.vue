1121c214335ceda19f5a95280db624928ba340c1
王爱彬 <hungeraibin@126.com>
2018-02-07 11:54:32 +0800

    带排行的song-list组件扩展和应用

--- src/components/top-list/top-list.vue
+++ src/components/top-list/top-list.vue
@@ -1,27 +1,65 @@
 <template>
   <transition name="slide">
-    <music-list :title="title" :bg-image="bgImage"></music-list>
+    <music-list :rank="rank" :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
 <script>
 import MusicList from 'components/music-list/music-list'
 import { mapGetters } from 'vuex'
+import {getMusicList} from 'api/rank'
+import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
 
 export default {
   components: {
     MusicList
+  },
+  data() {
+    return {
+      songs: [],
+      rank: true
+    }
+  },
+  created() {
+    this._getMusicList()
   },
   computed: {
     title() {
       return this.topList.topTitle
     },
     bgImage() {
-      return this.topList.picUrl
+      if (this.songs.length) {
+        return this.songs[0].image
+      }
+      return ''
     },
     ...mapGetters([
       'topList'
     ])
+  },
+  methods: {
+    _getMusicList() {
+      if (!this.topList.id) {
+        this.$router.push('/rank')
+        return
+      }
+      getMusicList(this.topList.id).then((res) => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSongs(res.songlist)
+        }
+      })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach((item) => {
+        const musicData = item.data
+        if (musicData.songid && musicData.albumid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
   }
 }
 </script>
