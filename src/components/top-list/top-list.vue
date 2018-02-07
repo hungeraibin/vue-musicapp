0271bba106b270729043bfa81d387e939ab4faba
王爱彬 <hungeraibin@126.com>
2018-02-07 11:24:49 +0800

    榜单详情也布局介绍及Vuex实现路由数据通讯

---
+++ src/components/top-list/top-list.vue
@@ -0,0 +1,36 @@
+<template>
+  <transition name="slide">
+    <music-list :title="title" :bg-image="bgImage"></music-list>
+  </transition>
+</template>
+
+<script>
+import MusicList from 'components/music-list/music-list'
+import { mapGetters } from 'vuex'
+
+export default {
+  components: {
+    MusicList
+  },
+  computed: {
+    title() {
+      return this.topList.topTitle
+    },
+    bgImage() {
+      return this.topList.picUrl
+    },
+    ...mapGetters([
+      'topList'
+    ])
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .slide-enter-active, .slide-leave-active 
+    transition: all 0.3s ease
+  
+  .slide-enter .slide-leave-to 
+    transform: translate3d(100%, 0, 0)
+</style>
+
